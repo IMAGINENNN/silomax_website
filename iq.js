2cd6b65d7645ed8ce13836e977239c73275a43b3
imaginennn <imaginennn@users.noreply.github.com>
2026-03-11 01:26:25 +0100

    Changed home page
    Add new 2 products pages.

---
+++ iq.js
@@ -0,0 +1,24 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    const elements = document.querySelectorAll(".fadeUp, .autoShow");
+
+    const observer = new IntersectionObserver((entries, observer) => {
+
+        entries.forEach(entry => {
+
+            if (entry.isIntersecting) {
+
+                entry.target.classList.add("show");
+
+                observer.unobserve(entry.target);
+            }
+
+        });
+
+    }, {
+        threshold: 0.2
+    });
+
+    elements.forEach(el => observer.observe(el));
+
+});
